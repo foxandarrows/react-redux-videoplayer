627d60f7f63ceb67b44f42f6c5a7fad1310bc807
saralitwak <sara.litwak@twipemobile.com>
2020-02-12 18:07:14 +0100

    Updates

---
+++ precache-manifest.ecfe2cd5b1ec60df5e3c2b803a035f84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba9545371950e8ac990454a96c31747",
+    "url": "/react-redux-videoplayer/index.html"
+  },
+  {
+    "revision": "f00800ec578909ec42e8",
+    "url": "/react-redux-videoplayer/static/js/2.c32fda0a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-redux-videoplayer/static/js/2.c32fda0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95fb4930145f41175ebf",
+    "url": "/react-redux-videoplayer/static/js/main.ae338f25.chunk.js"
+  },
+  {
+    "revision": "c8e8f3478fa41de275bf",
+    "url": "/react-redux-videoplayer/static/js/runtime-main.3a517d0f.js"
+  },
+  {
+    "revision": "eded1e233c76ec617c660a0759690341",
+    "url": "/react-redux-videoplayer/static/media/6play.eded1e23.png"
+  }
+]);
